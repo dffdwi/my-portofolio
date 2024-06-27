33380ea06d6c4516774329c6a33868f490ad02f7
DaffaDwi <daffadwi2003@gmail.com>
2024-06-27 08:38:48 +0700

    feat: Implement Swiper for OfferCard component

--- app/sections/About.jsx
+++ app/sections/About.jsx
@@ -4,6 +4,10 @@ import * as React from "react";
 import { useState, useEffect, useRef } from "react";
 import { Player } from "@lottiefiles/react-lottie-player";
 import AnimateOnView from "../Components/hooks/AnimateOnView";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
 
 import basicUiUx from "../../public/animations/responsive.json";
 import frontEndDeveloper from "../../public/animations/coding.json";
@@ -22,7 +26,7 @@ const OfferCard = ({ animationData, title, isActive }) => {
 
   return (
     <div
-      className={`flex flex-col px-6 py-5 w-full text-2xl font-semibold text-center text-violet-500 rounded-xl border border-violet-500 bg-violet-500 bg-opacity-10 max-md:px-3 max-md:py-3 transition-all duration-500 ${
+      className={`flex flex-col px-6 py-5 h-[370px] w-full text-2xl font-semibold text-center text-violet-500 rounded-xl border border-violet-500 bg-violet-500 bg-opacity-10 max-md:px-3 max-md:py-3 transition-all duration-500 ${
         isActive ? "opacity-100 scale-100" : "opacity-50 scale-95 blur-sm"
       }`}
       style={{ zIndex: isActive ? 2 : 1 }}
@@ -83,16 +87,8 @@ function About() {
           </h1>
         </AnimateOnView>
         <section className="flex flex-col items-center self-stretch mt-16 w-full max-md:mt-8">
-          <AnimateOnView direction="up" delay={0.5}>
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/2afee437a7d53b125fa78a05f1d3a889ed4e152d6fde7eaee833fc54916e57a1?apiKey=8f8528e23e2f43b68a1c2de0d919d80c&"
-              alt="Professional product illustration"
-              className="w-full max-w-md border-violet-500 border-10 rounded-md"
-            />
-          </AnimateOnView>
           <div className="mt-12 w-full max-w-4xl">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="hidden md:grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
               {offers.map((offer, index) => (
                 <AnimateOnView
                   key={index}
@@ -107,47 +103,80 @@ function About() {
                 </AnimateOnView>
               ))}
             </div>
+            <div className="block md:hidden">
+              <Swiper
+                effect="coverflow"
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={"auto"}
+                initialSlide={1}
+                coverflowEffect={{
+                  rotate: 50,
+                  stretch: 0,
+                  depth: 100,
+                  modifier: 1,
+                  slideShadows: true,
+                }}
+                onSlideChange={(swiper) =>
+                  setCurrentPage(swiper.activeIndex + 1)
+                }
+                modules={[EffectCoverflow]}
+              >
+                {offers.map((offer, index) => (
+                  <SwiperSlide key={index} style={{ width: "80%" }}>
+                    <OfferCard
+                      animationData={offer.animationData}
+                      title={offer.title}
+                      isActive={currentPage - 1 === index}
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
           </div>
-          <nav
-            className="flex gap-4 items-center mt-16 max-md:mt-8"
-            aria-label="Pagination"
-          >
-            <button
-              aria-label="Previous page"
-              onClick={handlePreviousPage}
-              disabled={currentPage === 1}
-              className="transition-all duration-300"
-            >
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/8a571cd7da88949485ba8a0873b13811dbf1dc28fb8a453526249f7cd69f6ac1?apiKey=8f8528e23e2f43b68a1c2de0d919d80c&"
-                alt=""
-                className="w-12"
-              />
-            </button>
-            {Array.from({ length: totalPages }, (_, i) => (
-              <div
-                key={i}
-                className={`h-2.5 rounded-full w-16 transition-all duration-500 ${
-                  i + 1 === currentPage
-                    ? "bg-violet-500 scale-110"
-                    : "bg-neutral-400 scale-100"
-                }`}
-              ></div>
-            ))}
-            <button
-              aria-label="Next page"
-              onClick={handleNextPage}
-              disabled={currentPage === totalPages}
-              className="transition-all duration-300"
-            >
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/42f13b50172df69cc0c1fe39cc74a7447208e082dae217ac75b5dff80db1a9e8?apiKey=8f8528e23e2f43b68a1c2de0d919d80c&"
-                alt=""
-                className="w-12"
-              />
-            </button>
+          <nav className="flex gap-4 items-center mt-16 max-md:mt-8">
+            <div className="justify-center w-full md:block hidden">
+              <button
+                aria-label="Previous page"
+                onClick={handlePreviousPage}
+                disabled={currentPage === 1}
+                className="transition-all duration-300"
+              >
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/8a571cd7da88949485ba8a0873b13811dbf1dc28fb8a453526249f7cd69f6ac1?apiKey=8f8528e23e2f43b68a1c2de0d919d80c&"
+                  alt=""
+                  className="w-12"
+                />
+              </button>
+            </div>
+            <div className="flex gap-4">
+              {Array.from({ length: totalPages }, (_, i) => (
+                <div
+                  key={i}
+                  className={`h-2.5 rounded-full w-16 transition-all duration-500 ${
+                    i + 1 === currentPage
+                      ? "bg-violet-500 scale-110"
+                      : "bg-neutral-400 scale-100"
+                  }`}
+                ></div>
+              ))}
+            </div>
+            <div className="justify-center w-full md:block hidden">
+              <button
+                aria-label="Next page"
+                onClick={handleNextPage}
+                disabled={currentPage === totalPages}
+                className="transition-all duration-300"
+              >
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/42f13b50172df69cc0c1fe39cc74a7447208e082dae217ac75b5dff80db1a9e8?apiKey=8f8528e23e2f43b68a1c2de0d919d80c&"
+                  alt=""
+                  className="w-12"
+                />
+              </button>
+            </div>
           </nav>
         </section>
       </main>
